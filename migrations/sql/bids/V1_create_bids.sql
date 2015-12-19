94ea9a11e96f9fab04401b76fc1a5134774f2c5f
Artem Prigoda <prigoda.artem@ya.ru>
2015-12-19 13:26:39 +0300

    Add a primary key for bids table

--- migrations/sql/bids/V1_create_bids.sql
+++ migrations/sql/bids/V1_create_bids.sql
@@ -1,9 +1,10 @@
 create table bids (
-  id          mediumint    not null auto_increment,
-  product     varchar(64)  not null,
-  amount      smallint     not null,
-  price       decimal(9,2) not null,
-  customer_id mediumint    not null,
-  place_time  timestamp    not null
+  id          int           not null auto_increment,
+  product     varchar(64)   not null,
+  amount      smallint      not null,
+  price       decimal(9, 2) not null,
+  customer_id mediumint     not null,
+  place_time  timestamp     not null,
+  primary key (id)
 );
 
