f450612bd414e4cf543ebb74af41f33cc518a7f1
Artem Prigoda <prigoda.artem@ya.ru>
2015-12-19 15:02:57 +0300

    Use bash scripts for creating and dropping databases
    Environment variables $OS_USER and $OS_PASSWORD should be set.
    MySQL is actually quite bad for parametrizing scripts ,but bash
    is a great tool for this job.

--- migrations/sql/init/create.sql
+++ migrations/sql/init/create.sql
@@ -1,27 +1,13 @@
--- TODO Get credentials from environment variables.
-set @os_user = ;
-set @os_password = ;
-
 create database customers;
 create database bids;
 create database fulfillments;
 create database contractors;
 
--- This is gross. Thanks, MySQL!
-set @query = concat('create user "',@os_user,'"@"localhost" identified by "',@os_password,'" ');
-prepare stmt from @query; execute stmt; deallocate prepare stmt;
-
-set @query = concat('grant select,insert,update,delete on customers.* to "',@os_user,'"@"localhost" ');
-prepare stmt from @query; execute stmt; deallocate prepare stmt;
-
-set @query = concat('grant select,insert,update,delete on bids.* to "',@os_user,'"@"localhost" ');
-prepare stmt from @query; execute stmt; deallocate prepare stmt;
-
-set @query = concat('grant select,insert,update,delete on fulfillments.* to "',@os_user,'"@"localhost" ');
-prepare stmt from @query; execute stmt; deallocate prepare stmt;
-
-set @query = concat('grant select,insert,update,delete on contractors.* to "',@os_user,'"@"localhost" ');
-prepare stmt from @query; execute stmt; deallocate prepare stmt;
+create user '<OS_USER>'@'localhost' identified by '<OS_PASSWORD>';
+grant select,insert,update,delete on customers.* to '<OS_USER>'@'localhost';
+grant select,insert,update,delete on bids.* to '<OS_USER>'@'localhost';
+grant select,insert,update,delete on fulfillments.* to '<OS_USER>'@'localhost';
+grant select,insert,update,delete on contractors.* to '<OS_USER>'@'localhost';
 
 flush privileges;
 
