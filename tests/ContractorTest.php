091f7ddad363fdf261a1b06ed604aff90a110008
Artem Prigoda <arteamon@gmail.com>
2015-12-20 20:00:53 +0300

    Add API for adding contractors
    Register an entity in the DB and return an URI that identifies it.

---
+++ tests/ContractorTest.php
@@ -0,0 +1,22 @@
+<?php
+require 'AppTest.php';
+
+class ContractorTest extends AppTest
+{
+    public function testCreateContractor()
+    {
+        $res = $this->client->request('POST', '/api/contractors/register');
+        $uri = $res->getBody()->getContents();
+        print $uri;
+
+        $this->assertEquals('201', $res->getStatusCode());
+        $cookie = $res->getHeader('Cookie');
+        $this->assertTrue($this->startsWith($cookie[0], 'os_session_id'));
+        $this->assertTrue($this->startsWith($uri, 'api/contractors/'));
+    }
+
+    private function startsWith($haystack, $needle)
+    {
+        return (substr($haystack, 0, strlen($needle)) === $needle);
+    }
+}
