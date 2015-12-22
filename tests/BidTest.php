2301a4696afe6b0029a33cf8b0afc1695bf7d4b9
Artem Prigoda <arteamon@gmail.com>
2015-12-22 16:12:05 +0300

    Require user to have a contractor session to get a list of bids

--- tests/BidTest.php
+++ tests/BidTest.php
@@ -6,7 +6,12 @@ class BidTest extends AppTest
 
     public function testGetBids()
     {
-        $res = $this->client->request('GET', '/api/bids');
+        $session_id = urlencode('3xnQd4CNGRBhTE48DqHHKZ+AZ7JDuGLu');
+        $res = $this->client->request('GET', '/api/bids', [
+            'headers' => [
+                'Cookie' => "cnt_session_id=$session_id"
+            ]
+        ]);
 
         $this->assertEquals('200', $res->getStatusCode());
         $this->assertEquals('application/json', $res->getHeaderLine('content-type'));
@@ -18,7 +23,12 @@ class BidTest extends AppTest
 
     public function testGetBidById()
     {
-        $res = $this->client->request('GET', 'api/bids/1');
+        $session_id = urlencode('3xnQd4CNGRBhTE48DqHHKZ+AZ7JDuGLu');
+        $res = $this->client->request('GET', 'api/bids/1', [
+            'headers' => [
+                'Cookie' => "cnt_session_id=$session_id"
+            ]
+        ]);
         $bid = json_decode($res->getBody());
 
         $this->assertEquals('200', $res->getStatusCode());
@@ -29,7 +39,11 @@ class BidTest extends AppTest
 
     public function testUnableFindBid()
     {
-        $res = $this->client->request('GET', 'api/bids/0', ['http_errors' => false]);
+        $session_id = urlencode('3xnQd4CNGRBhTE48DqHHKZ+AZ7JDuGLu');
+        $res = $this->client->request('GET', 'api/bids/0', [
+            'headers' => [
+                'Cookie' => "cnt_session_id=$session_id"
+            ], 'http_errors' => false]);
 
         $this->assertEquals('404', $res->getStatusCode());
         $this->assertEquals('application/json', $res->getHeaderLine('content-type'));
@@ -37,7 +51,10 @@ class BidTest extends AppTest
 
     public function testWrongId()
     {
-        $res = $this->client->request('GET', 'api/bids/one', ['http_errors' => false]);
+        $session_id = urlencode('3xnQd4CNGRBhTE48DqHHKZ+AZ7JDuGLu');
+        $res = $this->client->request('GET', 'api/bids/one', ['headers' => [
+            'Cookie' => "cnt_session_id=$session_id"
+        ], 'http_errors' => false]);
 
         $this->assertEquals('400', $res->getStatusCode());
         $this->assertEquals('application/json', $res->getHeaderLine('content-type'));
