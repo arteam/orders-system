12610e5bdc7ad054999865b1400ef8a3a0e3b352
Artem Prigoda <prigoda.artem@ya.ru>
2015-12-23 17:01:28 +0300

    Submit a bid to the server

--- public/components/js/customer-bids-place.js
+++ public/components/js/customer-bids-place.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
     // TODO checkIfSessionExists
 });
+
+$("#place-bid").click(function () {
+    var product = $("#product").val();
+    var amount = $("#amount").val();
+    var price = $("#price").val();
+    $.post('/api/bids/place', JSON.stringify({
+        "product": product,
+        "amount": amount,
+        "price": price
+    })).success(function () {
+        alert("Placed!")
+    }).fail(function () {
+        alert("Error");
+    });
+});
