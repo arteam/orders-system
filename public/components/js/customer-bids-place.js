9007923a798f0ad8d7738bdf7a0753d59fb27656
Artem Prigoda <arteamon@gmail.com>
2015-12-23 21:00:06 +0300

    Validate customer name on the bid form

--- public/components/js/customer-bids-place.js
+++ public/components/js/customer-bids-place.js
@@ -1,9 +1,15 @@
+var productRegexp = new RegExp('^[a-zA-Z0-9а-яА-Я\'"\s]+$');
+
 $(document).ready(function () {
     // TODO checkIfSessionExists
 });
 
 $("#place-bid").click(function () {
     var product = $("#product").val();
+    if (!validateProduct(product)) {
+        return;
+    }
+
     var amount = $("#amount").val();
     var price = $("#price").val();
     $.post('/api/bids/place', JSON.stringify({
@@ -16,3 +22,19 @@ $("#place-bid").click(function () {
         alert("Error");
     });
 });
+
+function validateProduct(product) {
+    if (product.length == 0) {
+        sweetAlert('Validation error', 'Product name is not set', 'error');
+        return false;
+    }
+    if (product.length > 32) {
+        sweetAlert('Validation error', 'Product name is too big', 'error');
+        return false;
+    }
+    if (!productRegexp.test(product)) {
+        sweetAlert('Validation error', 'Wrong product name format', 'error');
+        return false;
+    }
+    return true;
+}
