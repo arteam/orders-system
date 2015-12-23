4aa754c23a08c6ee19017e87c7593acb4b73ad46
Artem Prigoda <prigoda.artem@ya.ru>
2015-12-23 21:54:32 +0300

    Validate amount on the client-side

--- public/components/js/customer-bids-place.js
+++ public/components/js/customer-bids-place.js
@@ -1,4 +1,5 @@
 var productRegexp = new RegExp('^[a-zA-Z0-9а-яА-Я\'"\s]+$');
+var amountRegexp = new RegExp('^[0-9]+$');
 
 $(document).ready(function () {
     // TODO checkIfSessionExists
@@ -10,11 +11,15 @@ $("#place-bid").click(function () {
         return;
     }
 
-    var amount = $("#amount").val();
+    var textAmount = $("#amount").val();
+    if (!validateAmount(textAmount)) {
+        return;
+    }
+
     var price = $("#price").val();
     $.post('/api/bids/place', JSON.stringify({
         "product": product,
-        "amount": amount,
+        "amount": parseInt(textAmount),
         "price": price
     })).success(function () {
         alert("Placed!")
@@ -23,6 +28,11 @@ $("#place-bid").click(function () {
     });
 });
 
+/**
+ * Validates that product is set and contains safe symbols
+ * @param product
+ * @returns {boolean}
+ */
 function validateProduct(product) {
     if (product.length == 0) {
         sweetAlert('Validation error', 'Product name is not set', 'error');
@@ -34,6 +44,32 @@ function validateProduct(product) {
     }
     if (!productRegexp.test(product)) {
         sweetAlert('Validation error', 'Wrong product name format', 'error');
+        return false;
+    }
+    return true;
+}
+
+/**
+ * Validates that amount is a number between 1 and 1000
+ * @param textAmount
+ * @returns {boolean}
+ */
+function validateAmount(textAmount) {
+    if (textAmount.length == 0) {
+        sweetAlert('Validation error', 'Amount is not set', 'error');
+        return false;
+    }
+    if (!amountRegexp.test(textAmount)) {
+        sweetAlert('Validation error', 'Amount is not a number', 'error');
+        return false;
+    }
+    var amountAsNumber = parseInt(textAmount);
+    if (amountAsNumber < 1) {
+        sweetAlert('Validation error', 'Amount should be greater than 0', 'error');
+        return false;
+    }
+    if (amountAsNumber > 1000) {
+        sweetAlert('Validation error', 'Amount should be less than 100', 'error');
         return false;
     }
     return true;
