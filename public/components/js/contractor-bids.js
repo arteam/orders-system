d83e8e9a1ebdbf75b74064f00ef7648fe40c23e1
Artem Prigoda <arteamon@gmail.com>
2015-12-24 12:35:17 +0300

    Fade in new bids in the table

--- public/components/js/contractor-bids.js
+++ public/components/js/contractor-bids.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    localStorage.setItem('oldBidIds', []);
+    $.get("/api/bids", findBids);
     setInterval(function () {
         $.get("/api/bids", findBids)
     }, 2000);
@@ -6,18 +8,30 @@ $(document).ready(function () {
 
 function findBids(data) {
     var tbody = $("#bids-table").find("> tbody");
-    if (tbody.length > 0) {
-        // Clear bids
-        tbody.children().remove();
+
+    var oldBidIds = localStorage.getItem('oldBidIds');
+    if (oldBidIds == null) {
+        oldBidIds = [];
     }
 
-    // Add bids to the list
+    var ids = [];
+
+    // Add new bids to the list
     for (var i = 0; i < data.length; i++) {
-        tbody.append($('<tr>')
-            .append($('<td>').append(data[i].id))
-            .append($('<td>').append(data[i].product))
-            .append($('<td>').append(data[i].amount))
-            .append($('<td>').append(data[i].price))
-        );
+        var id = data[i].id;
+        console.log(id);
+        if (oldBidIds.indexOf(id) < 0) {
+            var tr = $('<tr>')
+                .append($('<td>').append(id))
+                .append($('<td>').append(data[i].product))
+                .append($('<td>').append(data[i].amount))
+                .append($('<td>').append(data[i].price));
+            tr.hide();
+            tr.fadeIn(2000);
+            tbody.append(tr);
+        }
+        ids.push(id);
     }
+
+    localStorage.setItem('oldBidIds', ids);
 }
