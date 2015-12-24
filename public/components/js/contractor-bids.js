d1c78d0bd355f68185eeabd08b18a6e12fff1fd6
Artem Prigoda <prigoda.artem@ya.ru>
2015-12-24 13:20:42 +0300

    Support for removing stale bids from the table in the UI

--- public/components/js/contractor-bids.js
+++ public/components/js/contractor-bids.js
@@ -1,37 +1,43 @@
 $(document).ready(function () {
-    localStorage.setItem('oldBidIds', []);
+    localStorage['currentBidIds'] = '';
     $.get("/api/bids", findBids);
     setInterval(function () {
         $.get("/api/bids", findBids)
     }, 2000);
 });
 
-function findBids(data) {
-    var tbody = $("#bids-table").find("> tbody");
+function findBids(bids) {
+    // Work with the bids table body
+    var tbody = $("#bids-table").find("tbody");
 
-    var oldBidIds = localStorage.getItem('oldBidIds');
-    if (oldBidIds == null) {
-        oldBidIds = [];
-    }
-
-    var ids = [];
+    // Grab current bid ids
+    var currentBidIds = localStorage['currentBidIds'] != '' ? JSON.parse(localStorage['currentBidIds']) : [];
 
     // Add new bids to the list
-    for (var i = 0; i < data.length; i++) {
-        var id = data[i].id;
-        console.log(id);
-        if (oldBidIds.indexOf(id) < 0) {
-            var tr = $('<tr>')
+    var newBidIds = [];
+    for (var i = 0; i < bids.length; i++) {
+        var id = parseInt(bids[i].id);
+        // If it's a new bid, add to the table with the "fade in" effect
+        if (currentBidIds.indexOf(id) < 0) {
+            tbody.append($('<tr>')
+                .attr('id', 'row' + id)
                 .append($('<td>').append(id))
-                .append($('<td>').append(data[i].product))
-                .append($('<td>').append(data[i].amount))
-                .append($('<td>').append(data[i].price));
-            tr.hide();
-            tr.fadeIn(2000);
-            tbody.append(tr);
+                .append($('<td>').append(bids[i].product))
+                .append($('<td>').append(bids[i].amount))
+                .append($('<td>').append(bids[i].price))
+                .fadeIn(2000));
         }
-        ids.push(id);
+        newBidIds.push(id);
     }
 
-    localStorage.setItem('oldBidIds', ids);
+    // Remove taken bids from the table with the "fade out" effect
+    for (var j = 0; j < currentBidIds.length; j++) {
+        var bidId = currentBidIds[j];
+        if (newBidIds.indexOf(bidId) < 0) {
+            $('#row' + bidId).fadeOut(2000);
+        }
+    }
+
+    // Sync the current bids ids
+    localStorage['currentBidIds'] = JSON.stringify(newBidIds);
 }
